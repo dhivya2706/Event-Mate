d24b191531e245a5365f2ac4a6143a28b86ba6ea
dhivya2706 <dhivyaprabha2706@gmail.com>
2026-03-23 19:12:39 +0530

    Removed unwanted folders

--- eventmate-backend/src/main/java/com/eventmate/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventmate.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.eventmate.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-
-    Admin findByEmail(String email);
-}
